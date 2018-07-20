ed3f99b5bae1e13296353f79caa27e90c9911238
Ricardo <rroque98@gmail.com>
2018-07-20 15:24:59 -0700

    Add server, route, controller, and model logic

--- server/app.js
+++ server/app.js
@@ -6,12 +6,12 @@ const app = express();
 
 app.set('port', process.env.PORT || 3002);
 
-app.get('/', function(req, res) {
-  res.redirect('/rooms/1');
-});
+// app.get('/', function(req, res) {
+//   res.redirect('/rooms/1');
+// });
 
 app.use(express.static('public/'));
-app.use(express.static('client/dist'));
+app.use( express.static('client/dist') );
 
 app.get('/rooms/:id', function(req, res) {
   const reactPath = path.join(__dirname, '../public/index.html');
