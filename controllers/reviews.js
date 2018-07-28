fa77a740955b40b0836d7c03527cbcea0a5493b8
Ricardo <rroque98@gmail.com>
2018-07-28 14:35:00 -0700

    Correct airbnb style guide errors

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,12 +3,12 @@ const models = require('../models');
 module.exports = {
   get: (req, res) => {
     // console.log('here is my req.params :', req.params.roomId)
-    models.Review.find({ 'room_id': parseInt(req.params.roomId) }, function(err, reviews) {
+    models.Review.find({ room_id: Number(req.params.roomId) }, (err, reviews) => {
       if (err) {
         res.status(500).send(err);
       } else {
         res.status(200).send(reviews);
       }
     });
-  }
-}
+  },
+};
